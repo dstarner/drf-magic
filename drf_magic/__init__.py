107eb574d4f9d90ba9647f56f8a9692200af1964
Dan Starner <dstarner@bloomberg.net>
2020-11-04 14:00:33 -0500

    bump version to 0.1.2

--- drf_magic/__init__.py
+++ drf_magic/__init__.py
@@ -1,14 +1,7 @@
-from .apps import MagicAppConfig
-
-__all__ = [
-    'MagicAppConfig',
-]
-
-
 __title__ = 'Django REST Framework Magic'
 __package__ = 'django-rest-magic'
 __docs__ = 'Increased functionality across permissions, serializers, viewsets, and more.'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Dan Starner'
 __license__ = 'BSD 3-Clause'
 __url__ = 'https://github.com/dstarner/drf-magic'
